fce89c469dc03aae1722add152d4656b89a38601
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2022-03-23 12:04:49 +0100

    commit

--- module/sheets/SPUNamedCharacterSheet.js
+++
@@ -1,9 +0,0 @@
-// Export document classes.
-export default class SPUActorSheet extends ActorSheet{
-  static get defaultOptions() {
-    return margeObject(super.defaultOptions), {
-      template: "systems/spu/templates/actor-sheet.html",
-      classes: ["spu", "sheet", "actor"]
-    }
-  }
-}
