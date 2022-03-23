11cbc0fb557002720e699bbea7b700d79fe25874
TsubakiO9 <tsubaki.t.r.9@gmail.com>
2022-03-23 11:53:51 +0100

    commit

--- spu.js
+++ spu.js
@@ -1,7 +1,7 @@
 // Import sheet classes.
 import { spu } from "./module/config.js";
-import SPUItemSheet from "./module/sheets/SPUItemSheet.js";
-import SPUNamedCharacterSheet from "./module/sheets/SPUNamedCharacterSheet.js";
+import { SPUItemSheet } from "./module/sheets/SPUItemSheet.js";
+import { SPUNamedCharacterSheet } from "./module/sheets/SPUNamedCharacterSheet.js";
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
